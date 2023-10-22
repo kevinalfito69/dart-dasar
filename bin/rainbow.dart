880b1fbd31bf7f2e8c102ca1c01146cf12b6df85
kevinalfito69 <kevinalfito69@gmail.com>
2023-10-23 00:36:50 +0700

    enum

---
+++ bin/rainbow.dart
@@ -0,0 +1,9 @@
+
+import "rainbow_color.dart";
+void main(){
+  print(Rainbow.values);
+  print(Rainbow.blue.index);
+  print(Rainbow.blue.name);
+  print(Rainbow.blue);
+
+}
---
+++ bin/rainbow_color.dart
@@ -0,0 +1,4 @@
+// Enums mewakili kumpulan konstan yang membuat kode kita lebih jelas dan mudah dibaca.
+enum Rainbow{
+  red,blue,orange,yellow,green
+}
